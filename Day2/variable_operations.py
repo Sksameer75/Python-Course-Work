a7f5d12ee00a36571515bd5384d8f061e0e3c27f
Sk Sameer Basha <101972421+Sksameer75@users.noreply.github.com>
2026-08-05 17:54:05 +0530

    Python Intro and Keywords

---
+++ Day2/variable_operations.py
@@ -0,0 +1,36 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+======= RESTART: C:/Users/Admin/Desktop/python_course_work/Day2/Day-2.py =======
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+======= RESTART: C:/Users/Admin/Desktop/python_course_work/Day2/Day-2.py =======
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> 
+======= RESTART: C:/Users/Admin/Desktop/python_course_work/Day2/Day-2.py =======
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+['_', 'case', 'match', 'type']
+>>> a = 10
+>>> a
+10
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c = 10,20,30
+>>> a
+10
+>>> print(a,b,c)
+10 20 30
+>>> a = 10
+>>> b = 50
+>>> a,b = b,a
+>>> a
+50
+>>> b
+10
