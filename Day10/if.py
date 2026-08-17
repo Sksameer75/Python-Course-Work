3c6feaf42b34469a28a56f9c80e0821f26ca6776
Sameer Basha Shaik <101972421+Sksameer75@users.noreply.github.com>
2026-08-17 17:52:35 +0530

    Conditional Statements

---
+++ Day10/if.py
@@ -0,0 +1,16 @@
+#simple if 
+'''sales = int(input("Enter the slaes:"))
+if sales>1000:
+    print("Best Seller")
+
+
+eli_acc = eval(input("Eligible Account:"))
+ver_sub = eval(input("Meta Verified subscription:"))
+
+if eli_acc and ver_sub:
+    print("Verified Badge Granted")
+'''
+    
+rain_status = eval(input("Enter the rain status:"))
+if rain_status:
+    print("Extra Rain Charges Applied")
